5b2284d040bf99b93df0958ad067723286dceb2d
Edouard Grave <egrave@devfair0134.h2.fair>
2021-02-01 06:31:42 -0800

    new src/data.py

---
+++ src/data.py
@@ -0,0 +1,210 @@
+import torch
+import random
+import json
+import numpy as np
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self,
+                 data,
+                 n_context,
+                 question_prefix='question:',
+                 title_prefix='title:',
+                 passage_prefix='context:'):
+        self.data = data
+        self.n_context = n_context
+        self.question_prefix = question_prefix
+        self.title_prefix = title_prefix
+        self.passage_prefix = passage_prefix
+
+    def __len__(self):
+        return len(self.data)
+
+    # TODO(egrave): space before EOS ?
+    def get_target(self, example):
+        if 'target' in example:
+            return example['target'] + '</s>'
+        elif 'answers' in example:
+            return random.choice(example['answers']) + '</s>'
+        else:
+            return None
+
+    def __getitem__(self, index):
+        example = self.data[index]
+        question = self.question_prefix + example['question']
+        target = self.get_target(example)
+
+        if 'ctxs' in example and self.n_context is not None:
+            f = self.title_prefix + " {} " + self.passage_prefix + " {}"
+            contexts = example['ctxs'][:self.n_context]
+            passages = [f.format(c['title'], c['text']) for c in contexts]
+            scores = [float(c['score']) for c in contexts]
+            scores = torch.tensor(scores)
+            # TODO(egrave): do we want to keep this?
+            if len(contexts) == 0:
+                contexts = [question]
+        else:
+            contexts, scores = None, None
+
+        return {
+            'index' : index,
+            'question' : question,
+            'target' : target,
+            'passages' : passages,
+            'scores' : scores
+        }
+
+    ## TODO(egrave): use python sort
+    def sort_data(self):
+        if self.n_context is None or not 'score' in self.data[0]['ctxs'][0]:
+            return
+        for ex in self.data:
+            scores = [ctx['score'] for ctx in ex['ctxs']]
+            idx = np.argsort(-np.array(scores))
+            ctxs = [ex['ctxs'][k] for k in idx]
+            ex['ctxs'] = ctxs
+
+    def get_example(self, index):
+        return self.data[index]
+
+def encode_passages(batch_text_passages, tokenizer, max_length):
+    passage_ids, passage_masks = [], []
+    for k, text_passages in enumerate(batch_text_passages):
+        p = tokenizer.batch_encode_plus(
+            text_passages,
+            max_length=max_length,
+            pad_to_max_length=True,
+            return_tensors='pt',
+            truncation=True
+        )
+        passage_ids.append(p['input_ids'][None])
+        passage_masks.append(p['attention_mask'][None])
+
+    passage_ids = torch.cat(passage_ids, dim=0)
+    passage_masks = torch.cat(passage_masks, dim=0)
+    # TODO(egrave): check .bool() is ok
+    return passage_ids, passage_masks.bool()
+
+class Collator(object):
+    def __init__(self, text_maxlength, tokenizer):
+        self.tokenizer = tokenizer
+        self.text_maxlength = text_maxlength
+
+    def __call__(self, batch):
+        assert(batch[0]['target'] != None)
+        index = torch.tensor([ex['index'] for ex in batch])
+        #question = [ex['question'] for ex in batch]
+        target = [ex['target'] for ex in batch]
+        target = self.tokenizer.batch_encode_plus(
+            target,
+            pad_to_max_length=True,
+            return_tensors="pt"
+        )
+        target_ids = target["input_ids"]
+        target_mask = target["attention_mask"].bool()
+
+        #text_passages = [ex['passages'] for ex in batch]
+        def append_question(example):
+            return [example['question'] + " " + t for t in example['passages']]
+        text_passages = [append_question(example) for example in batch]
+        passage_ids, passage_masks = encode_passages(text_passages,
+                                                     self.tokenizer,
+                                                     self.text_maxlength)
+
+        return (index, target_ids, target_mask, passage_ids, passage_masks)
+
+def load_data(data_path=None, global_rank=-1, world_size=-1, maxload=-1):
+    assert data_path
+    if data_path.endswith('.jsonl'):
+        data = open(data_path, 'r')
+    elif data_path.endswith('.json'):
+        with open(data_path, 'r') as fin:
+            data = json.load(fin)
+    examples = []
+    for k, example in enumerate(data):
+        if global_rank > -1 and not k%world_size==global_rank:
+            continue
+        if maxload > -1 and k >= maxload:
+            break
+        if data_path is not None and data_path.endswith('.jsonl'):
+            example = json.loads(example)
+        if not 'id' in example:
+            example['id'] = k
+        if not 'score' in example:
+            example['score'] = 1.0 / (k + 1)
+        examples.append(example)
+    ## egrave: is this needed?
+    if data_path is not None and data_path.endswith('.jsonl'):
+        data.close()
+    return examples
+
+class RetrieverCollator(object):
+    def __init__(self, tokenizer, passage_maxlength=200, question_maxlength=40):
+        self.tokenizer = tokenizer
+        self.passage_maxlength = passage_maxlength
+        self.question_maxlength = question_maxlength
+
+    def __call__(self, batch):
+        index = torch.tensor([ex['index'] for ex in batch])
+
+        question = [ex['question'] for ex in batch]
+        question = self.tokenizer.batch_encode_plus(
+            question,
+            pad_to_max_length=True,
+            return_tensors="pt",
+            max_length=self.question_maxlength,
+            truncation=True
+        )
+        question_ids = question['input_ids']
+        question_mask = question['attention_mask'].bool()
+
+        if batch[0]['scores'] is None or batch[0]['passages'] is None:
+            return index, question_ids, question_mask, None, None, None
+
+        scores = [ex['scores'] for ex in batch]
+        scores = torch.stack(scores, dim=0)
+
+        passages = [ex['passages'] for ex in batch]
+        passage_ids, passage_masks = encode_passages(
+            passages,
+            self.tokenizer,
+            self.passage_max_length
+        )
+
+        return (index, question_ids, question_mask, passage_ids, passage_masks, scores)
+
+class TextDataset(torch.utils.data.Dataset):
+    def __init__(self,
+                 data,
+                 title_prefix='title:',
+                 passage_prefix='context:'):
+        self.data = data
+        self.title_prefix = title_prefix
+        self.passage_prefix = passage_prefix
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        example = self.data[index]
+        text = self.title_prefix + " " + example[2] + " " + \
+            self.passage_prefix + " " + example[1]
+        return example[0], text
+
+class TextCollator(object):
+    def __init__(self, tokenizer, maxlength=200):
+        self.tokenizer = tokenizer
+        self.maxlength = maxlength
+
+    def __call__(self, batch):
+        index = [x[0] for x in batch]
+        encoded_batch = self.tokenizer.batch_encode_plus(
+            [x[1] for x in batch],
+            pad_to_max_length=True,
+            return_tensors="pt",
+            max_length=self.maxlength,
+            truncation=True
+        )
+        text_ids = encoded_batch['input_ids']
+        text_mask = encoded_batch['attention_mask']
+
+        return index, text_ids, text_mask

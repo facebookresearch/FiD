dde9805e3b4dfcf40eddf1ff5538ea38fc7ddf75
gizacard <gizacard@fb.com>
2021-02-05 03:48:41 -0800

    remove resize in training loop

--- train_reader.py
+++ train_reader.py
@@ -48,8 +48,8 @@ def train(model, optimizer, scheduler, step, train_dataset, eval_dataset, opt, c
                 model.encoder.n_passages = n_passages
 
             train_loss = model.forward(
-                input_ids=context_ids.cuda(), #.view(context_ids.size(0), -1),
-                attention_mask=context_mask.cuda(), #.view(context_ids.size(0), -1),
+                input_ids=context_ids.cuda(),
+                attention_mask=context_mask.cuda(),
                 labels=labels.cuda()
             )[0]
 
@@ -106,8 +106,8 @@ def evaluate(model, dataset, tokenizer, collator, opt):
             model.encoder.n_passages = context_ids.size(1)
 
             outputs = model.generate(
-                input_ids=context_ids.cuda(), #.view(context_ids.size(0), -1), 
-                attention_mask=context_mask.cuda(), #.view(context_mask.size(0), -1), 
+                input_ids=context_ids.cuda(),
+                attention_mask=context_mask.cuda(),
                 max_length=50
             )
 
